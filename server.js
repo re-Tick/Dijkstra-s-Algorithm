d22def589059597e591cfbf46ebbba4b4d700c54
re-Tick10 <jain.ritik.1001@gmail.com>
2021-05-30 20:13:14 +0530

    Host project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname+ "/index.html");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+    console.log('Server Started at 3000');
+})
